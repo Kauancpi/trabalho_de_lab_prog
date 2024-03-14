6629442d07dabcb35579ce86e7115e22ceb0439a
Kauancpi <kauancpi@gmail.com>
2024-03-14 10:03:25 -0300

    eu nao sei mais

--- teste.c
+++ teste.c
@@ -19,9 +19,35 @@ typedef struct disciplinas{
 
 
 int main(){
-    char continuar="y";
-    while(continuar=="y" || continuar=="Y"){
+    char continuar='y';
+    FILE *txt;
+    int ano;
+    char titulo_txt[15];
+    char sano[4];
+    char speriodo[15];
+    int periodo;
+    while(continuar=='y' || continuar=='y'){
         
+        scanf("%d %d",&ano,&periodo);
+        
+        
+        sprintf(sano,"%d",ano);
+        sprintf(speriodo,"%d",periodo);
+        strcpy(titulo_txt,sano);
+        strcat(titulo_txt,"_");
+        strcat(titulo_txt,speriodo);
+        strcat(titulo_txt,".txt");
+
+        printf("%s",titulo_txt);
+        
+        txt = fopen(titulo_txt,"w");
+        fwrite(titulo_txt,1,sizeof(titulo_txt),txt);
+        
+        
+        scanf("%c",&continuar);
+
+
+
          
     }
 }
